93c8ace0ed0e12ecdf7b17af069693656fddef81
ATPCOXBAU <93449158+ATPCOXBAU@users.noreply.github.com>
2025-07-28 22:58:22 -0600

    Semana 1

---
+++ Semana 1/Sintaxis.js
@@ -0,0 +1,48 @@
+// JAVASCRIPT CASE-SENSITIVE 
+
+let variable1 = "variable1"
+let Variable1 = "Variable Mayuscula"
+
+console.log(variable1)
+console.log(Variable1)
+
+// PUNTO y COMA 
+
+let variable2 = "variable1";
+let Variable2 = "Variable Mayuscula";
+console.log(variable2);
+console.log(Variable2);
+
+
+// Identificadores Validos 
+
+
+let _variable = 1;
+let $variale = 2;
+let camelCase = 3; 
+let CONSTANT = 4;
+
+
+// Identificadores Invalidos 
+
+// let 2variable = 1; # No inicio Numero
+// let mi-variale = 1; # No Guiones 
+// let class = 3; # No palabras reservadas
+
+// Palabras Reservadas 
+// let, const var, function, class, if , else, for, while
+//  enum, interface, package, private, public 
+
+
+//Comentarios 
+
+
+// Comentario una linea, inicio "//"
+
+
+/*
+    Este es un comentario de 
+    Bloque 
+    XD 
+*/
+
